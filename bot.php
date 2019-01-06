c28914eb4c6c1d7065667f6f87dcf31c4c3eb02a
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2019-01-06 20:56:16 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,8 @@
 <?php
 echo "token : ";
 $token=trim(fgets(STDIN));
+echo "sign :";
+$sign=trim(fgets(STDIN));
 echo "jumlah : ";
 $jml=trim(fgets(STDIN));
 echo "sleep : ";
@@ -10,21 +12,21 @@ while($x<$jml){
 sleep($wait);
 $x++;
 flush();
-echo "Suksess";
+echo " ==> Suksess   ";
 
 
 
 }
 
-function video($token,$jml,$wait){
-        $rand=(1, 99999);
+function video($token,$sign,$jml,$wait){
+ 
         $ch = curl_init;
         curl_setopt($ch, CURLOPT_URL, "https://api.yogopro.com/api/reward/watchVideo");
         curl_setopt($ch, CURLPOT_RETURNTRNASFER, 1);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
         $result=curl_exec($ch);
-        return $result;
+        echo $result."\n";
         sleep($wait);
         $x++;
         flush();
